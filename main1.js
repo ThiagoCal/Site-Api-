c0e1ab6eaaa3f4487ef29a66e358b3d2da3e41ac
ThiagoCal <42551348+ThiagoCal@users.noreply.github.com>
2021-09-10 16:07:03 -0300

    Delete main1.js

--- main1.js
+++
@@ -1,7 +0,0 @@
-import { handleNovoItem } from "./components/criaTarefa.js";
-import { carregaTarefa } from "./components/carregaTarefa.js";
-const newTask = document.querySelector('[data-form-button]');
-
-newTask.addEventListener('click', handleNovoItem);
-
-carregaTarefa();
